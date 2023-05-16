0deade7c9ad1ed7901bd1c06a23fe408b50cd97d
BEK <bek.akbarjonov@gmail.com>
2023-05-16 18:04:40 -0400

    first commit- committing all files to the repository

---
+++ ConsoleApp15/WeighingMachine.cs
@@ -0,0 +1,67 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+public class WeighingMachine
+{
+    public WeighingMachine(double precision)
+    {
+        Precision = precision;
+    }
+
+    public double Precision { get; }
+
+
+    private double weight;
+    public double Weight
+    {
+
+        get { return weight; }
+
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Your number cant be negative");
+            }
+            weight = value;
+        }
+    }
+
+    public int adj;
+    
+    public int Tareadjustment
+    {
+        get { return adj; }
+        
+        set { adj = value+5;  }
+    }
+
+    public double Display
+    {
+        get { return weight - adj; }
+        set { Console.WriteLine(value); }
+    }
+         
+   
+
+
+
+}
+   
+
+
+
+        
+
+        //set value negative
+
+
+    
+
+
+
+
+
+
+
+
---
+++ ConsoleApp15/program.cs
@@ -0,0 +1,11 @@
+﻿
+
+WeighingMachine weighing=new WeighingMachine(3);
+weighing.Weight = 60.5;
+Console.WriteLine(weighing.Weight);
+
+
+weighing.Tareadjustment = 5;
+Console.WriteLine(weighing.Tareadjustment);
+
+Console.WriteLine(weighing.Display);
